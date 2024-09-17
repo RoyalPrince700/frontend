50be6755dfff21c0c026376a0cdb2f84149074a4
Royal Joseph <royalprincecube@gmail.com>
2024-09-17 02:15:09 +0100

    update checkout section adding total price and quantity and payment button

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -180,9 +180,11 @@ const Cart = () => {
                 }
               };
               
-        
-        
-
+    const totalQty = data.reduce((previousValue,currentValue)=>
+         previousValue + currentValue.quantity, 0)
+    const totalPrice = data.reduce(
+        (preve,curr)=>preve + (curr.quantity * 
+            curr?.productId?.sellingPrice) , 0)
 
   return (
     <div className='container mx-auto'>
@@ -231,8 +233,12 @@ const Cart = () => {
                         <h2 className='text-lg lg:text-xl text-ellipsis
                        line-clamp-1 '>{product?.productId?.productName}</h2>
                        <p className='capitalize text-slate-500'>{product?.productId?.category}</p>
-                   <p className='text-red-600 font-medium text-lg'>
-                    {displayNARCurrency(product?.productId?.sellingPrice)}</p>
+              <div className='flex items-center justify-between'>
+              <p className='text-red-600 font-medium text-lg'>
+              {displayNARCurrency(product?.productId?.sellingPrice)}</p>
+              <p className='text-slate-600 font-semibold text-lg'>
+              {displayNARCurrency(product?.productId?.sellingPrice * product?.quantity)}</p>
+              </div>
                     <div className='flex items-center gap-3 mt-1'>
                         <button className='flex rounded hover:bg-red-600
                          hover:text-white justify-center items-center 
@@ -267,11 +273,24 @@ const Cart = () => {
                                 
                             </div>
                 ) : (
-                    <div className='h-36 bg-slate-200'>
-                    Total
-                </div>
-                )
-            }
+        <div className='h-36 bg-white'>
+        <h2 className='text-white bg-red-600 px-4 py-1'>Summary</h2>
+        <div className='flex items-center justify-between 
+        px-4 font-medium text-lg text-slate-600  gap-2'>
+            <p>Quantity</p>
+            <p>{totalQty}</p> 
+            </div>
+
+            <div className='flex 
+            font-medium text-lg text-slate-600 items-center justify-between px-4  gap-2'>
+                <p>Total Price</p>
+                <p>{displayNARCurrency(totalPrice)}</p>
+            </div>
+            <button className='bg-blue-600 p-2 text-white
+            w-full'>Payment</button>
+        </div>
+    )
+}
          </div>
           
 
