a84b2898b3f5878b23cab74899fab47d971db511
Royal Joseph <royalprincecube@gmail.com>
2024-09-17 22:57:29 +0100

    when i click logout it takes user to home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,6 +33,7 @@ const Header = () => {
     if (data.success) {
       toast.success(data.message);
       dispatch(setUserDetails(null));
+      navigate("/")
     }
     if (data.error) {
       toast.error(data.message);
